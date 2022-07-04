27279fb04d4cb290cf753b09d82d193eb9918c47
Carlos Andrés Osorio <caaosorioal@unal.edu.co>
2022-07-03 20:42:21 -0500

    Update mapper.py

--- mapper.py
+++ mapper.py
@@ -78,7 +78,7 @@ def get_mean_value_f(clusters):
 
 if __name__ == '__main__':
     # Select the files and the path
-    path = '/Users/carlosandresosorioalcalde/Documents/GitHub/topological-data-analysis/data'
+    path = '/Users/carlosandresosorioalcalde/'
     file = 'Saber_11__2019-2.csv'
 
     # Read the data with chunks
